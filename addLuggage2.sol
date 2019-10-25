2e0bc04ec0fb6a2d2dfe761619b3e1bfe2549bfd
Samyak S Sarnayak <34161949+Samyak2@users.noreply.github.com>
2019-10-25 16:41:26 +0530

    Updated with luggage tracking

--- addLuggage2.sol
+++ addLuggage2.sol
@@ -2,129 +2,108 @@ pragma solidity ^0.5.0;
 
 contract addLuggage2{
     string public name;
-    // uint public luggageCount;
-    // mapping(uint => Luggage) public luggages;
     mapping(string => Luggage) private luggageStore;
-    mapping(address => mapping(string => sentrecieved)) private walletStore;
+    mapping(string => mapping(string => sentrecieved)) private walletStore;
 
     struct Luggage{
         uint weight;
         bool initialized;
+        string airport_initial;
     }
 
     struct sentrecieved {
         bool sent;
-        bool recieved;
+        string airport_sent;
+        bool received;
+        string airport_received;
     }
 
-    // event LuggageAdded (
-    //     uint id,
-    //     string owner_name,
-    //     uint price,
-    //     uint weight,
-    //     address payable owner,
-    //     bool sent,
-    //     uint airport_id,
-    //     string airport_name
-    // );
+    event AssetCreate(string account, string uuid, uint weight);
+    event RejectCreate(string account, string uuid, string message);
+    event AssetTransfer(string from, string to, string uuid);
+    event RejectTransfer(string from, string to, string uuid, string message);
 
-    event AssetCreate(address account, string uuid, uint weight);
-    event RejectCreate(address account, string uuid, string message);
-    event AssetTransfer(address from, address to, string uuid);
-    event RejectTransfer(address from, address to, string uuid, string message);
-
-    function createAsset(string memory uuid, uint weight) public {
- 
+    function createAsset(string memory uuid, uint weight, string memory airport_initial) public {
         if(luggageStore[uuid].initialized) {
-            emit RejectCreate(msg.sender, uuid, "Asset with this UUID already exists.");
+            emit RejectCreate(airport_initial, uuid, "Asset with this UUID already exists.");
             return;
         }
-    
-        luggageStore[uuid] = Luggage(weight, true);
+
+        luggageStore[uuid] = Luggage(weight, true, airport_initial);
         sentrecieved memory a;
         a.sent = false;
-        a.recieved = false;
-        walletStore[msg.sender][uuid] = a;
-        emit AssetCreate(msg.sender, uuid, weight);
+        a.received = true;
+        walletStore[airport_initial][uuid] = a;
+        emit AssetCreate(airport_initial, uuid, weight);
     }
-
-    // function transferAsset(address to, string memory uuid) public {
-    //     if(!luggageStore[uuid].initialized) {
-    //         emit RejectTransfer(msg.sender, to, uuid, "No asset with this UUID exists");
-    //         return;
-    //     }
-    //     if(walletStore[msg.sender][uuid]) {
-    //         emit RejectTransfer(msg.sender, to, uuid, "Sender does not own this asset.");
-    //         return;
-    //     }
-    //     walletStore[msg.sender][uuid] = false;
-    //     walletStore[to][uuid] = true;
-    //     emit AssetTransfer(msg.sender, to, uuid);
-    // }
-    function sendAsset(address to, string memory uuid) public {
+    
+    function sendAsset(string memory airport_from, string memory airport_to, string memory uuid) public {
         if(!luggageStore[uuid].initialized) {
-            emit RejectTransfer(msg.sender, to, uuid, "No asset with this UUID exists");
+            emit RejectTransfer(airport_from, airport_to, uuid, "No asset with this UUID exists");
             return;
         }
-        if(!walletStore[msg.sender][uuid].recieved) {
-            emit RejectTransfer(msg.sender, to, uuid, "Sender did not recieve this luggage.");
+        if(!walletStore[airport_from][uuid].received) {
+            emit RejectTransfer(airport_from, airport_to, uuid, "Sender did not recieve this luggage.");
             return;
         }
-        walletStore[msg.sender][uuid].sent = true;
+        walletStore[airport_from][uuid].sent = true;
+        walletStore[airport_from][uuid].airport_sent = airport_to;
         // walletStore[to][uuid] = true;
-        emit AssetTransfer(msg.sender, to, uuid);
+        emit AssetTransfer(airport_from, airport_to, uuid);
     }
 
-    function recieveAsset(address to, string memory uuid) public {
+    function recieveAsset(string memory airport_from, string memory airport_to, string memory uuid) public {
         if(!luggageStore[uuid].initialized) {
-            emit RejectTransfer(msg.sender, to, uuid, "No asset with this UUID exists");
+            emit RejectTransfer(airport_from, airport_to, uuid, "No asset with this UUID exists");
             return;
         }
-        // if(!walletStore[msg.sender][uuid].recieved) {
-        //     emit RejectTransfer(msg.sender, to, uuid, "Sender did not recieve this luggage.");
-        //     return;
-        // }
-        walletStore[msg.sender][uuid].recieved = true;
+        if(!walletStore[airport_from][uuid].sent) {
+            emit RejectTransfer(airport_from, airport_to, uuid, "Sender did not send the luggage");
+            return;
+        }
+        walletStore[airport_to][uuid].received = true;
+        walletStore[airport_to][uuid].airport_received = airport_from;
         // walletStore[to][uuid] = true;
-        emit AssetTransfer(msg.sender, to, uuid);
+        emit AssetTransfer(airport_from, airport_to, uuid);
     }
 
-    function getAssetByUUID(string memory uuid) public view returns (uint) {
-        return (luggageStore[uuid].weight);
+    function getAssetByUUID(string memory uuid) public view returns (uint, string memory) {
+        return (luggageStore[uuid].weight, luggageStore[uuid].airport_initial);
     }
 
-    function isOwnerOf(address owner, string memory uuid) public view returns (bool) {
-        if(walletStore[owner][uuid].recieved && !walletStore[owner][uuid].sent) {
+    function isOwnerOf(string memory owner, string memory uuid) public view returns (bool) {
+        if(walletStore[owner][uuid].received && !walletStore[owner][uuid].sent) {
             return true;
         }
         return false;
+    }
+
+    function append(string memory a, string memory b, string memory c, string memory d, string memory e, string memory f) internal pure returns (string memory) {
+        return string(abi.encodePacked(a, b, c, d, e, f));
+    }
+
+    function trackLuggage(string memory uuid) public view returns (string memory)  {
+        string memory cur_airport = luggageStore[uuid].airport_initial;
+        string memory output = "";
+        sentrecieved memory cur_sr = walletStore[luggageStore[uuid].airport_initial][uuid];
+        while(cur_sr.sent) {
+            output = append(output, "Luggage sent from ",cur_airport," to ",cur_sr.airport_sent,"\n");
+            if(walletStore[cur_sr.airport_sent][uuid].received) {
+                output = append(output, "Luggage received at ",cur_sr.airport_sent," from ",walletStore[cur_sr.airport_sent][uuid].airport_received,"\n");
+            }
+            cur_airport = cur_sr.airport_sent;
+            cur_sr = walletStore[cur_airport][uuid];
+        }
+        return output;
+    }
 }
 
-    // function addLuggage(string memory _owner_name, uint _price, uint _weight, string memory _airport_name, uint _airport_id) public {
-    //     // Require a valid name
-    //     require(bytes(_owner_name).length > 0, "Invalid Owner name");
-
-    //     // Require a valid price
-    //     require(_price > 0,"Invalid Price");
-
-    //     //Require a valid weight
-    //     require(_weight>0,"Invalid Weight");
-
-    //     //Require a valid airport name
-    //     require(bytes(_airport_name).length > 0,"Invalid airport name");
-
-    //     //Require a valid id
-    //     require(_airport_id>0, "Invalid ID");
-        
-    //     // Make sure parameters are correct
-    //     // Increment Luggage count
-    //     luggageCount ++;
-        
-    //     // Create the product
-    //     luggages[luggageCount] = Luggage(luggageCount, _owner_name, _price, _weight, msg.sender, true, _airport_name, _airport_id);
-        
-    //     // Trigger an event
-    //     emit LuggageAdded(luggageCount, _owner_name, _price, _weight, msg.sender, true, _airport_name, _airport_id);
-
-    // }
-}
+// addluggage2 = await addLuggage2.deployed()
+// ac = await web3.eth.getAccounts()
+// addluggage2.createAsset("123", 123, "Bangalore")
+// addluggage2.isOwnerOf("Bangalore", "123")
+// addluggage2.sendAsset("Bangalore", "Chennai", "123")
+// addluggage2.isOwnerOf("Chennai", "123")
+// addluggage2.recieveAsset("Bangalore", "Chennai", "123")
+// addluggage2.isOwnerOf("Chennai", "123")
+// addluggage2.trackLuggage("123")
